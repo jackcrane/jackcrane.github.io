268d28adbe6f943d6d5e0118791bb881eceda38b
Jack <3jbc22@gmail.com>
2019-04-07 16:02:53 -0400

    ssh

--- projectwisp/script.js
+++ projectwisp/script.js
@@ -1,16 +1,18 @@
 const SLIDES_MAP = {
 	0: "WiSP_conceptLogo.jpg",
-	1: "1.png",
+	1: "1.PNG",
 	2: "2.jpg",
 	3: "3.png",
 	4: "4.png",
 	5: "5.png",
+	6: "6.jpg",
 }
 function numberToSrc(number){
 	console.log(SLIDES_MAP[number])
 	document.getElementById("slideshow").src = SLIDES_MAP[number]
 }
 var currentSlide = 0
+var maxSlides = 6
 
 function lastslide() {
 	if(currentSlide!=0) {
@@ -18,14 +20,14 @@ function lastslide() {
 		numberToSrc(currentSlide)
 		console.log(currentSlide)
 	} else {
-		currentSlide = 5
+		currentSlide = maxSlides
 		numberToSrc(currentSlide)
 		console.log(currentSlide)
 	}
 }
 
 function nextslide() {
-	if(currentSlide!=5) {
+	if(currentSlide!=maxSlides) {
 		currentSlide += 1
 		numberToSrc(currentSlide)
 		console.log(currentSlide)
