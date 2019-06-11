edb336366a23a3cfcb019f94459673575f3bf8c9
Jack <3jbc22@gmail.com>
2019-06-10 20:46:40 -0400

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use SendGrid\Mail\Content;
+use SendGrid\Mail\From;
+use SendGrid\Mail\Mail;
+use SendGrid\Mail\To;
+
+$sendgrid = new SendGrid("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");
+$email    = new SendGrid\Mail\Mail();
+
+$email- >addTo("3jbc22@gmail.com")
+      ->setFrom("aether@aether.com")
+      ->setSubject("Sending with SendGrid is Fun")
+      ->setHtml("and easy to do anywhere, even with PHP");
+
+echo $email;
+
+$sendgrid->send($email);
+
+?>
