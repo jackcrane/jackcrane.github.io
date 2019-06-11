befcb65205e252a636a4ddb0afdb8ff35590a7dd
Jack <3jbc22@gmail.com>
2019-06-10 20:50:07 -0400

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -10,7 +10,7 @@ use SendGrid\Mail\To;
 $sendgrid = new SendGrid("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");
 $email    = new SendGrid\Mail\Mail();
 
-$email- >addTo("3jbc22@gmail.com")
+$email->addTo("3jbc22@gmail.com")
       ->setFrom("aether@aether.com")
       ->setSubject("Sending with SendGrid is Fun")
       ->setHtml("and easy to do anywhere, even with PHP");
