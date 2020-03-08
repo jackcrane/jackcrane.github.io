f4b4c4e59eb6dc2803bc6c767dd805560ce962b2
jackcrane <3jbc22@gmail.com>
2020-03-08 17:10:49 -0400

    educated site update

--- educated_site/script.js
+++ educated_site/script.js
@@ -23,3 +23,18 @@ function showlearnmore() {
 	document.getElementById("learnmore").hidden = false
 	document.getElementById("learnmore").style.opacity = "1"
 }
+
+function verifyCookies() {
+	if(!navigator.cookieEnabled) {
+		document.getElementsByClassName("warn")[0].hidden = false;
+		let inputs = document.getElementsByTagName("input")
+		inputs = Array.from(inputs)
+		inputs.forEach(element => element.disabled = "true")
+	}
+}
+
+verifyCookies()
+
+function cookieRsn() {
+	alert("Educated uses cookies to verify a user has not voted on a site before. This helps to protect the integrity of our results.\n\nIf you are not okay with this, fear not. Just do not submit, and we will never use cookies anywhere else")
+}
