6b625e4294edb4b25356c62f35ff911ebcc340c8
jackcrane <3jbc22@gmail.com>
2020-06-03 13:32:02 -0400

    ugh

--- educated_site/script.js
+++ educated_site/script.js
@@ -16,7 +16,7 @@ setTimeout(draw, 300)
 function pagechange(page) {
 	document.getElementsByClassName("main")[0].style.opacity = "0"
     erase()
-    setTimeout(function(){window.location.href=page + ".html"},3500)
+    setTimeout(function(){window.location.href=page},3500)
 }
 
 function showlearnmore() {
@@ -30,6 +30,9 @@ function verifyCookies() {
 		let inputs = document.getElementsByTagName("input")
 		inputs = Array.from(inputs)
 		inputs.forEach(element => element.disabled = "true")
+		inputs = document.getElementsByTagName("select")
+		inputs = Array.from(inputs)
+		inputs.forEach(element => element.disabled = "true")
 	}
 }
 
@@ -37,4 +40,27 @@ verifyCookies()
 
 function cookieRsn() {
 	alert("Educated uses cookies to verify a user has not voted on a site before. This helps to protect the integrity of our results.\n\nIf you are not okay with this, fear not. Just do not submit, and we will never use cookies anywhere else")
+}
+var url
+var rank
+function submitVerify() {
+	url = document.getElementById("url").value.toString()
+	rank = document.getElementById("rank").value.toString()
+	
+	if(getCookie(url)==undefined) {
+		console.log("ok, writing")
+		document.cookie = window['url'] + " ="+rank+"; expires=Thu, 18 Dec 2099 12:00:00 UTC";
+		document.getElementById("valid").value = "true"
+	} else {
+		console.log("already present")
+		document.getElementById("valid").value = "false"
+	}
+
+	document.getElementsByTagName("form")[0].submit()
+}
+
+function getCookie(name) {
+	var value = "; " + document.cookie;
+	var parts = value.split("; " + name + "=");
+	if (parts.length == 2) return parts.pop().split(";").shift();
 }
